23dca2ee295ffe047c098875a02d75ed6e3922ac
smolbrain <randomlyrandamu@gmail.com>
2023-01-09 02:10:09 +1100

    Initial

---
+++ controllers/build/CMakeFiles/controllers.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/controllers.dir/controller.cpp.o"
+  "CMakeFiles/controllers.dir/controller.cpp.o.d"
+  "controllers"
+  "controllers.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/controllers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
