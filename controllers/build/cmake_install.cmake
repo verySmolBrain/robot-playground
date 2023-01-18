6ba764553e10eb77bb291beb69e692acacef21fb
smolbrain <randomlyrandamu@gmail.com>
2023-01-18 22:26:10 +1100

    Testing Lidar

--- controllers/build/cmake_install.cmake
+++ controllers/build/cmake_install.cmake
@@ -39,7 +39,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "/usr/bin/x86_64-w64-mingw32-objdump")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
--- controllers/controller.cpp
+++ controllers/controller.cpp
@@ -1,21 +1,44 @@
 #include <webots/Robot.hpp>
 #include <webots/DistanceSensor.hpp>
+#include <webots/Lidar.hpp>
+#include <string.h>
 #include <iostream>
+#include <fstream>
 
 #define TIME_STEP 32
 
 using namespace webots;
+using namespace std;
 
 int main() {
     Robot *robot = new Robot();
 
-    DistanceSensor *sensor = robot->getDistanceSensor("ds");
-    sensor->enable(TIME_STEP);
+    Lidar *lidar = robot->getLidar("Hokuyo URG-04LX-UG01");
+    lidar->enable(TIME_STEP);
+    lidar->enablePointCloud();
 
     while (robot->step(TIME_STEP) != -1) {
-        std::cout << "Distance: " << sensor->getValue() << std::endl;
+        const LidarPoint *points = lidar->getPointCloud();
+
+
     }
 
     delete robot;
     return 0;
+}
+
+void outputPointCloud(Lidar *lidar) {
+    const string filename = "point_cloud.csv";
+
+    ofstream file(filename);
+
+    if (file.is_open()) {
+        const LidarPoint *points = lidar->getPointCloud();
+
+        for (int i = 0; i < lidar->getNumberOfPoints(); i++) {
+            file << points[i].x << "," << points[i].y << "," << points[i].z << endl;
+        }
+
+        file.close();
+    }
 }
